038078534bfe9ec339151b691afd59da8abb181d
WuYimin <wym.missyou@gmail.com>
2015-03-16 10:31:02 +0800

    simple calculator
    1.基本的四则运算实现；
    2.自动布局：适配各种屏幕，按钮尺寸自动变化，充满屏幕；
    3.syntax:  performOperation ({$0+$1}) ，functional param

--- ViewController.swift
+++ ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var display: UILabel!
 
-    var userInputedANumber : Bool = false
+    var userInputedANumber = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,7 +24,7 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     @IBAction func digtal(sender: UIButton) {
         let digtal = sender.currentTitle
         if userInputedANumber {
@@ -33,8 +33,65 @@ class ViewController: UIViewController {
             display.text = digtal!
             userInputedANumber = true
         }
+    }
+    
+    
+    var operatorStack = Array<NSString>()
+    
+    @IBAction func operate(sender: UIButton) {
         
+        enter()
+        
+        operatorStack.append(sender.currentTitle!)
+        println("operatorStack = \(operatorStack)")
     }
 
+    var operandStack = Array<Double>()
+    
+    @IBAction func enter() {
+       userInputedANumber = false
+        operandStack.append(displayValue)
+       
+        if(operatorStack.count==1 &&  operandStack.count==2){
+            performCalculat()
+        }else if(operandStack.count == operatorStack.count + 1){
+            //do nothing
+        }else{
+            operandStack.removeAll(keepCapacity: true)
+            operatorStack.removeAll(keepCapacity: true)
+        }
+        
+        println("operandStack = \(operandStack)")
+    }
+    
+    
+    func performCalculat() -> Double{
+       let operatorS = operatorStack.removeLast()
+        switch operatorS {
+            case "+": performOperation ({$0+$1})
+            case "-":performOperation ({$1-$0})
+            case "*":performOperation ({$0*$1})
+            case "/":performOperation ({$1/$0})
+            default : break
+        }
+        println("perform calculat result \(displayValue)")
+        return displayValue
+    }
+    
+    func performOperation(operation : (op1: Double,op2:Double) -> Double){
+        if(operandStack.count>=2){
+              displayValue  =  operation(op1: operandStack.removeLast(),op2: operandStack.removeLast())
+        }
+    }
+    
+    var displayValue:Double{
+        get{
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        set{
+            userInputedANumber = false
+            display.text = "\(newValue)"
+        }
+    }
 }
 
